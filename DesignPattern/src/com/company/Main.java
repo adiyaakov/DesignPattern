7b607b682820990ed78a08df279aa69d08d365f6
adiyaakov <adiyaakov0105@gmail.com>
2017-04-01 20:32:58 +0300

    :)

--- DesignPattern/src/com/company/Main.java
+++ DesignPattern/src/com/company/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
 	    //Builder ex:
-        MyBuilder builder = new MyBuilder();
-        builder.setHight(2,3).setColor("red");
+        MyBuilder myBuilder = new MyBuilder();
+        myBuilder.setHight(2,3).setColor("red");
 
 
         //Singletone ex:
